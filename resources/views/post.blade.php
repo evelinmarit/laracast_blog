4becc7678e45e6dce7c36ed869463c995bedc94c
Evelin Maripuu <evelin.marit@gmail.com>
2024-05-20 22:54:18 +0300

    First 7 lessons

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My blog</title>
+        <link rel="stylesheet" href="/app.css">
+
+    </head>
+
+    <body>
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, voluptas? Saepe quod optio minima at inventore officia ipsa quibusdam facilis fugiat sequi quos eum, maiores laboriosam eveniet tenetur architecto ratione?
+            </p>
+        </article>
+
+        <a href="/">Go Back</a>
+    </body>
