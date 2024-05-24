7b7bbe0ba4a5b757fb91b009fa8995ad16ea6239
Evelin Maripuu <evelin.marit@gmail.com>
2024-05-24 23:00:51 +0300

    Blade Basic, Caching, episode 14

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
+//use Illuminate\Cache\CacheManager;$Cache;
 
 class Post
 {
@@ -27,6 +28,8 @@ class Post
     }
 
     public static function all() {
+        return cache()->rememberForever('posts.all', function () {
+
         return collect(File::files(resource_path("posts")))
         ->map(fn($file) => YamlFrontMatter::parseFile($file))
         ->map(fn($document) => new Post(
@@ -35,8 +38,11 @@ class Post
             $document->date,
             $document->body(),
             $document->slug,
-        ));
-    }
+        ))
+        ->sortByDesc('date');
+    });
+
+}
 
     public static function find($slug) {
         return static::all()->firstWhere('slug', $slug);
