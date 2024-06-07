d4f73d50385ebdd56fade19fe84bbeb6d2d3f2df
Evelin Maripuu <evelin.marit@gmail.com>
2024-06-08 00:16:32 +0300

    Search, episode 38

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
@@ -7,20 +8,8 @@ use App\Models\User;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File;
 
-Route::get('/', function() {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-
-Route::get("posts/{post:slug}", function (Post $post) {
-
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get("posts/{post:slug}", [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
