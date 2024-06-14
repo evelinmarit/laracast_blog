6eba411d03cd485927f059173c84841353b30a44
Evelin Maripuu <evelin.marit@gmail.com>
2024-06-14 11:43:25 +0300

    Failed Validation and Old Input Data, episode 47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,8 +16,13 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1"´>{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -31,8 +36,14 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required
                     >
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1"´>{{ $message }}</p>
+                @enderror
+            </div>
                 </div>
 
                 <div class="mb-6">
@@ -46,8 +57,14 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email') }}"
                         required
                     >
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1"´>{{ $message }}</p>
+                @enderror
+            </div>
                 </div>
 
                 <div class="mb-6">
@@ -63,6 +80,11 @@
                         id="password"
                         required
                     >
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1"´>{{ $message }}</p>
+                @enderror
+            </div>
                 </div>
 
                 <div class="mb-6">
@@ -72,6 +94,14 @@
                         Submit
                     </button>
                 </div>
+
+                @if ($errors->any())
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li class="text-red-500 text-xs">{{ $error }}</li>
+                @endforeach
+                </ul>
+                @endif
 
             </form>
         </main>
