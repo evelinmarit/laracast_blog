21dc0459abca63bed614c98833878a1d620b84c1
Evelin Maripuu <evelin.marit@gmail.com>
2024-06-14 11:17:19 +0300

    Password Hashing - uuel laravelil juba sees, episode 46

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,10 @@ class User extends Authenticatable
         ];
     }
 
+    public function setPasswordAttribute($password) {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts() {
         return $this->hasMany(Post::class);
     }
